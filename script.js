64438a50b6c651860b780dae883452ff907924a7
Arjun cv <arjun7180@gmail.com>
2024-03-03 00:11:21 +0530

    added set intervels and animations using javaScript on developer text in header

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+//buttons
 const popup = document.querySelector('.popup');
 const bookNow = document.querySelectorAll('.book-now');
 const discover = document.querySelector('.discover')
-
-
-
-
 const sectionTours = document.querySelector('#section-tours');
 
-discover.addEventListener('click', () => {
+//components
+const subHeading = document.querySelector('.primery-title--sub');
 
+
+
+//cursor
+// const dev = ['D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'];
+
+const devHtml = document.querySelector('.dev');
+
+const dev = 'DEVELOPER ';
+const length = dev.length;
+
+devHtml.innerHTML = '';
+let i = 0;
+let devCount = 0;
+let isDev = true;
+
+function startAnimLeftToRight() {
+
+    const devAnim = setInterval(() => {
+
+        let dev = '';
+
+        if (devCount % 2 === 0) dev = 'DEVELOPER ';
+
+        else dev = 'Web Dev ';
+
+        devHtml.innerHTML = dev.slice(0, i++);
+
+        if (i === dev.length) {
+            clearInterval(devAnim);
+            i = 0;
+            devCount++;
+            setTimeout(() => {
+                startAnimLeftToRight();
+            }, 3000);
+        };
+
+    }, 500);
+
+}
+startAnimLeftToRight();
+
+//Event listeners
+discover.addEventListener('click', () => {
     sectionTours.scrollIntoView({ block: 'center', behavior: "smooth" })
 });
